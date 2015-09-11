e4fb61e7b8365f4bb4b34665f03b3dc7fb2689ff
mczech <mczech@mail.uni-paderborn.de>
2015-09-11 19:52:45 +0200

    rpc

--- PyPRSVT/ranking/rpc.py
+++ PyPRSVT/ranking/rpc.py
@@ -1,12 +1,39 @@
 import pandas as pd
+import ast
+from itertools import combinations
 
-def learn_ranking_function(data):
-    pass
+class RPC(object):
+
+    def __init__(self, labels, base, base_options):
+        bin_clfs = {}
+        for rel in combinations(labels, 2):
+            bin_clfs['{0} >= {1}'.format(rel[0], rel[1])] \
+                = base(base_options)
+        self.bin_clfs = bin_clfs
+        self.base = base
+
+    def __reverse_rel(self, rel):
+        rel_parts = [r.strip() for r in rel.split('>=')]
+        return '{0} >= {1}'.format(rel_parts[1], rel_parts[0])
+
+    def fit(self, X, y):
+        """
+        Todo
+        :param X:
+        :param y:
+        :return:
+        """
+        for rel in self.bin_clfs.keys():
+            y_rel = []
+            for rel_set in y:
+                rel_set = ast.literal_eval(rel_set)
+                if rel in rel_set:
+                    y_rel.append(1)
+                elif self.__reverse_rel(rel):
+                    y_rel.append(0)
+                else:
+                    y_rel.append(None)
 
 
-def split_into_binary_problems():
-    pass
-
-
-def solve_binary_problem():
-    pass
+    def predict(self):
+        pass
