43470d79e7ed4d3a672dc16bee894dce1a0b836d
mczech <mczech@mail.uni-paderborn.de>
2015-09-21 17:47:32 +0200

    bugfix

--- PyPRSVT/preprocessing/ranking.py
+++ PyPRSVT/preprocessing/ranking.py
@@ -2,7 +2,7 @@
 Python module for preprocessing software verification competition results to solve ranking problems
 """
 import pandas as pd
-from itertools import permutations
+from itertools import combinations
 
 
 def create_benchmark_ranking_df(results, compare_results):
@@ -12,7 +12,7 @@ def create_benchmark_ranking_df(results, compare_results):
     :param compare_results:
     :return:
     """
-    benchmarks_permutations = list(permutations(results.keys(), 2))
+    benchmark_combinations = list(combinations(results.keys(), 2))
     df = pd.concat(results, axis=1)
     # rows with na values give us not information, so drop them.
     df.dropna(inplace=True)
@@ -21,11 +21,11 @@ def create_benchmark_ranking_df(results, compare_results):
     for row in df.iterrows():
         preferences = []
         sourcefile, results_df = row
-        for pair in benchmarks_permutations:
+        for pair in benchmark_combinations:
             tool_a, tool_b = pair
             c = compare_results(results_df[tool_a], results_df[tool_b])
             if c == 1 or c == 0:
                 preferences.append('{0} >= {1}'.format(tool_a, tool_b))
         ret_df.set_value(sourcefile, 'ranking', preferences)
-    return ret_df
+    return ret_df, results.keys()
 
