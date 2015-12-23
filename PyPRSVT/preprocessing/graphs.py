2edd487bf86b6a2c31e5c802f0561eead29d09d4
zenscr <zenscr@gmail.com>
2015-12-23 23:02:29 +0100

    adapted graph preprocessing, moved files

--- PyPRSVT/preprocessing/graphs.py
+++ PyPRSVT/preprocessing/graphs.py
@@ -17,6 +17,10 @@ def _run_cpachecker(self, path_to_source):
     cpash_path = join(self.path_to_cpachecker, 'scripts', 'cpa.sh')
     output_path = join(self.path_to_cpachecker, 'output')
     graph_path = join(self.path_to_cpachecker, 'output', 'vtask_graph.dot')
+    node_labels_path = join(self.path_to_cpachecker, 'output', 'nodes.labels')
+    edge_types_path = join(self.path_to_cpachecker, 'output', 'edge_types.labels')
+    edge_truth_path = join(self.path_to_cpachecker, 'output', 'edge_truth.labels')
+    node_depths_path = join(self.path_to_cpachecker, 'output', 'edge_depths.labels')
     if not isdir(self.path_to_cpachecker):
         raise ValueError('CPAChecker directory not found')
     if not (isfile(path_to_source) and (path_to_source.endswith('.i') or path_to_source.endswith('.c'))):
@@ -27,8 +31,34 @@ def _run_cpachecker(self, path_to_source):
         raise ValueError('Invalid output of CPAChecker.')
     if match_vresult.group(1) != 'TRUE':
         raise ValueError('CFALabels Analysis failed:' + out)
-    assert isfile(graph_path), 'Invalid output of CPAChecker'
-    return graph_path
+    assert isfile(graph_path), 'Invalid output of CPAChecker: Missing graph output'
+    assert isfile(node_labels_path), 'Invalid output of CPAChecker: Missing node labels output'
+    assert isfile(edge_types_path), 'Invalid output of CPAChecker: Missing edge types output'
+    assert isfile(edge_truth_path), 'Invalid output of CPAChecker: Missing edge truth values output'
+    assert isfile(node_depths_path), 'Invalid output of CPAChecker: Missing node depths output'
+    return graph_path, node_labels_path, edge_types_path, edge_truth_path, node_depths_path
+
+
+def _read_node_labeling(labels_path):
+    labels = {}
+    with open(labels_path) as f:
+        for line in f:
+            m = re.match(r"([0-9]+),([A-Z_]+)\n", line)
+            if m is not None:
+                node = m.group(1)
+                labels[node] = m.group(2)
+    return labels
+
+
+def _read_edge_labeling(labels_path):
+    labels = {}
+    with open(labels_path) as f:
+        for line in f:
+            m = re.match(r"([0-9]+),([0-9]+),\[(([A-Z_\s]+,?)+)\]\n", line)
+            if m is not None:
+                edge = (m.group(1), m.group(2), 0)
+                labels[edge] = [x.strip() for x in m.group(2).split(',')]
+    return labels
 
 
 def create_graph_df(vtask_paths, graphs_dir_out):
@@ -44,13 +74,26 @@ def create_graph_df(vtask_paths, graphs_dir_out):
     data = []
     for vtask in vtask_paths:
         ret_path = join(graphs_dir_out, basename(vtask) + '.dot')
+
         # DEBUG
         if isfile(ret_path):
             data.append(ret_path)
             continue
-        graph_path = _run_cpachecker(abspath(vtask))
+
+        graph_path, node_labels_path, edge_types_path, edge_truth_path, node_depths_path \
+            = _run_cpachecker(abspath(vtask))
         nx_digraph = nx.read_dot(graph_path)
+        node_labels = _read_node_labeling(node_labels_path)
+        nx.set_node_attributes(nx_digraph, 'label', node_labels)
+        edge_types = _read_edge_labeling(edge_types_path)
+        nx.set_edge_attributes(nx_digraph, 'types', edge_types)
+        edge_truth = _read_edge_labeling(edge_truth_path)
+        nx.set_edge_attributes(nx_digraph, 'truth', edge_truth)
+        node_depths = _read_node_labeling(node_depths_path)
+        nx.set_node_attributes(nx_digraph, 'depth', node_depths)
+
         assert not isfile(ret_path)
         nx.write_dot(nx_digraph, ret_path)
         data.append(ret_path)
+
     return pd.DataFrame(data, index=vtask_paths)
