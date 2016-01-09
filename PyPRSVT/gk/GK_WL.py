b66c5284a10dfa6cb3e8a5c9b5f861c6ceff9faf
zenscr <zenscr@gmail.com>
2016-01-09 16:41:53 +0100

    now the graph kernel is MUCH faster

--- PyPRSVT/gk/GK_WL.py
+++ PyPRSVT/gk/GK_WL.py
@@ -4,6 +4,7 @@ Weisfeiler Lehman graph kernel for CFGs
 import numpy as np
 import networkx as nx
 from tqdm import tqdm
+import time
 
 
 class GK_WL(object):
@@ -18,18 +19,16 @@ class GK_WL(object):
             self.__label_counter__ += 1
         return self.__label_lookup__[long_label]
 
-    def _collect_labels(self, node, i, graph, it, node_labels, node_depth, types, D):
+    def _collect_labels(self, node, i, graph, it, node_labels, node_depth, types, D, edge_types, edge_truth):
         ret = []
-        edge_types = nx.get_edge_attributes(graph, 'type')
-        edge_truth = nx.get_edge_attributes(graph, 'truth')
         for e in graph.in_edges_iter(nbunch=node, keys=True):
             # todo add for multigraph
             source, _, _ = e
-            edge_t = edge_types[e]
+            edge_t = edge_types[i][e]
             if edge_t in types and node_depth[i][source] <= D:
                 long_edge_label = "_".join(
                         [str(t) for t in [node_labels[it][i][source],
-                                          self._compress(str(edge_t)), self._compress(str(edge_truth[e]))]])
+                                          self._compress(str(edge_t)), self._compress(str(edge_truth[i][e]))]])
                 # long_edge_label = "_".join(
                 #         [str(t) for t in [node_labels[it][i][source],
                 #                           str(edge_t), str(edge_truth[e])]])
@@ -61,6 +60,8 @@ class GK_WL(object):
         all_graphs_number_of_nodes = 0
         node_labels = [0] * (h+1)
         node_depth = [0] * len(graph_list)
+        edge_types = [0] * len(graph_list)
+        edge_truth = [0] * len(graph_list)
 
         for it in range(h+1):
             node_labels[it] = [0] * len(graph_list)
@@ -69,6 +70,8 @@ class GK_WL(object):
             node_labels[0][i] = {key: self._compress(value)
                                  for key, value in nx.get_node_attributes(g, 'label').items()}
             node_depth[i] = nx.get_node_attributes(g, 'depth')
+            edge_types[i] = nx.get_edge_attributes(g, 'type')
+            edge_truth[i] = nx.get_edge_attributes(g, 'truth')
             all_graphs_number_of_nodes += len([node for node in nx.nodes_iter(g) if node_depth[i][node] <= D])
             # if i == 0:
             #     self._graph_to_dot(g, node_labels[0][i], "graph{}.dot".format(i))
@@ -97,7 +100,7 @@ class GK_WL(object):
                 node_labels[it][i] = {}
                 for node in g.nodes_iter():
                     if node_depth[i][node] <= D:
-                        label_collection = self._collect_labels(node, i, g, it-1, node_labels, node_depth, types, D)
+                        label_collection = self._collect_labels(node, i, g, it-1, node_labels, node_depth, types, D, edge_types, edge_truth)
                         long_label = "_".join(str(x) for x in [np.concatenate([np.array([node_labels[it-1][i][node]]),
                                                                np.sort(label_collection)])])
                         node_labels[it][i][node] = self._compress(long_label)
